0609a0316055965b7e700f2604f24ba7f36feea3
IvTole <toledano.ivan16@gmail.com>
2025-01-21 14:15:30 -0600

    main script run_clustering.py has been added

---
+++ modules/run_clustering.py
@@ -0,0 +1,21 @@
+# Libraries
+
+# Standard
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+# Matplotlib figures to annotate
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import Circle
+
+# Astropy
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy import units as u
+
+# Spectral cubes
+from spectral_cube import SpectralCube
+
+import math
+import scipy
