3cde5517566d104c676e21d13215d92dd632460e
IvTole <toledano.ivan16@gmail.com>
2025-02-04 22:53:27 -0600

    Added mom8 fits and plots

--- modules/module_utils.py
+++ modules/module_utils.py
@@ -6,6 +6,11 @@ import scipy
 import math
 import os
 
+import matplotlib.pyplot as plt 
+from matplotlib.colors import LogNorm
+from matplotlib.colors import PowerNorm
+from matplotlib.ticker import LogFormatter
+
 # Astropy
 from astropy.io import fits
 from astropy.wcs import WCS
@@ -44,7 +49,7 @@ def smooth(image, kern_px=1):
     f1=scipy.ndimage.gaussian_filter(image, kern_px/(2*math.sqrt(2*math.log(2))))
     return f1
 
-def cube_mom8(cube,velmin,velmax):
+def cube_mom8(cube_path,velmin,velmax,output_path,write_fits=False):
     """
     Returns the moment 8 (max intensity) image of a data cube.
 
@@ -57,11 +62,17 @@ def cube_mom8(cube,velmin,velmax):
         moment(SpectralCube 2d): Moment 8 image (2d) of the data cube.
     """
 
+    cube = SpectralCube.read(cube_path)
+    cube.allow_huge_operations = True
     cube_slab = cube.spectral_slab(velmin *u.km / u.s, velmax *u.km / u.s)
     moment = cube_slab.max(axis = 0)
+
+    if write_fits:
+        moment.write(output_path, overwrite=True)
+
     return moment
 
-def cube_mom0(cube,velmin,velmax):
+def cube_mom0(cube_path,velmin,velmax,output_path,write_fits=False):
     """
     Returns the moment 0 (mean intensity) image of a data cube.
 
@@ -74,11 +85,16 @@ def cube_mom0(cube,velmin,velmax):
         moment(SpectralCube 2d): Moment 0 image (2d) of the data cube.
     """
 
+    cube = SpectralCube.read(cube_path)
     cube_slab = cube.spectral_slab(velmin*u.km/u.s, velmax*u.km/u.s)
     moment = cube_slab.with_spectral_unit(u.km/u.s).moment(order=0)
+
+    if write_fits:
+        moment.write(output_path, overwrite=True)
+        
     return moment
 
-def cube_smoothing(data_path, output_path, source_prefix, emission_prefix, kernel_px=1, write_fits=False):
+def cube_smoothing(data_path, output_path, prefix_source, prefix_emission, kernel_px=1, write_fits=False):
     hdu = fits.open(data_path)[0]
     for v in range(0,hdu.data.shape[0]):
         hdu.data[v,:,:] = smooth(hdu.data[v,:,:],kern_px=kernel_px)
@@ -86,6 +102,43 @@ def cube_smoothing(data_path, output_path, source_prefix, emission_prefix, kerne
     
     if write_fits:
         print('Writing new cube in following path:', output_path)
-        hdu.writeto(os.path.join(output_path,source_prefix+'_'+emission_prefix+'_smoothed.fits'),
+        hdu.writeto(os.path.join(output_path,prefix_source+'_'+prefix_emission+'_smoothed.fits'),
                 overwrite = True)
+
+def plot_mom8(path, output_path, prefix_source, prefix_emission, gamma=1.0, vmin=0.0, vmax=25.0):
+    hdu = fits.open(path)[0]
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111, projection = WCS(hdu.header))
+
+    im = ax.imshow(hdu.data, cmap = 'RdBu_r',
+                   norm = PowerNorm(gamma=gamma, vmin=vmin, vmax=vmax))
+
+    ### Axis parameters ###
+    lat = ax.coords['glat']
+    lat.set_axislabel('Galactic Latitude', size = 12, alpha = 1.0)
+    lat.set_ticks(width = 1, spacing = 0.1 * u.deg)
+    lat.set_ticklabel(size = 12, exclude_overlapping=True)
+    lat.display_minor_ticks(True)
+
+    lon = ax.coords['glon']
+    lon.set_axislabel('Galactic Longitude', size = 12, alpha = 1.0)
+    lon.set_ticks(width = 1, spacing = 0.1 * u.deg)
+    lon.set_ticklabel(size = 12, exclude_overlapping=True)
+    lon.display_minor_ticks(True)
+
+    ### Annotations ###
+    ax.annotate(prefix_source + ', ' + prefix_emission + ' Peak Temperature', xy = (5,5), xytext = (5, 5), color='black',
+            fontsize = 8, bbox = dict(boxstyle = "round", fc = "w", alpha = 0.0))
+
+    ### Colorbar ###
+    cbar = plt.colorbar(im, pad=.01)
+    cbar.set_label(r'$T_{\rm MB}^{\rm \ peak}$', labelpad = 4, y = 0.5, rotation=90, size = 14)
+    #cbar.ax.tick_params(labelsize=14)
+    #cbar.ax.locator_params(nbins=6)
+
+    plt.savefig(os.path.join(output_path, prefix_source + '_' + prefix_emission + '_mom8.pdf'),
+                bbox_inches = 'tight')
+    plt.close()
     
--- modules/run_clustering.py
+++ modules/run_clustering.py
@@ -23,7 +23,7 @@ import scipy
 
 # External modules
 from module_data_path import cube_data_path, plot_data_path, fits_data_path
-from module_utils import rms, smooth, cube_mom0, cube_mom8, cube_smoothing
+from module_utils import rms, smooth, cube_mom0, cube_mom8, cube_smoothing, plot_mom8
 
 def main():
 
@@ -38,12 +38,33 @@ def main():
     data_path_c18o = os.path.join(data_path, 'dr21_c18o_cube.fits')
 
     # Prefix
-    source_prefix = 'dr21'
+    prefix_source = 'dr21'
     
     # Smoothing (for all emissions)
-    cube_smoothing(data_path=data_path_12co, output_path=fits_path, source_prefix=source_prefix, emission_prefix='12co', kernel_px=1, write_fits=True)
-    cube_smoothing(data_path=data_path_13co, output_path=fits_path, source_prefix=source_prefix, emission_prefix='13co', kernel_px=1, write_fits=True)
-    cube_smoothing(data_path=data_path_c18o, output_path=fits_path, source_prefix=source_prefix, emission_prefix='c18o', kernel_px=1, write_fits=True)
+    cube_smoothing(data_path=data_path_12co, output_path=fits_path, prefix_source=prefix_source, prefix_emission='12co', kernel_px=1, write_fits=True)
+    cube_smoothing(data_path=data_path_13co, output_path=fits_path, prefix_source=prefix_source, prefix_emission='13co', kernel_px=1, write_fits=True)
+    cube_smoothing(data_path=data_path_c18o, output_path=fits_path, prefix_source=prefix_source, prefix_emission='c18o', kernel_px=1, write_fits=True)
+
+    # Smoothed cubes path
+    data_sm_path_12co = os.path.join(fits_path, f'{prefix_source}_12co_smoothed.fits')
+    data_sm_path_13co = os.path.join(fits_path, f'{prefix_source}_13co_smoothed.fits')
+    data_sm_path_c18o = os.path.join(fits_path, f'{prefix_source}_c18o_smoothed.fits')
+
+    # Make mom8 fits files (using smoothed cubes)
+    cube_mom8(cube_path=data_sm_path_12co, velmin=-50.0, velmax=50.0, output_path=os.path.join(fits_path, f'{prefix_source}_12co_mom8.fits'), write_fits=True)
+    cube_mom8(cube_path=data_sm_path_13co, velmin=-50.0, velmax=50.0, output_path=os.path.join(fits_path, f'{prefix_source}_13co_mom8.fits'), write_fits=True)
+    cube_mom8(cube_path=data_sm_path_c18o, velmin=-50.0, velmax=50.0, output_path=os.path.join(fits_path, f'{prefix_source}_c18o_mom8.fits'), write_fits=True)
+
+    # Mom8 fits path
+    data_mom8_path_12co = os.path.join(fits_path, f'{prefix_source}_12co_mom8.fits')
+    data_mom8_path_13co = os.path.join(fits_path, f'{prefix_source}_13co_mom8.fits')
+    data_mom8_path_c18o = os.path.join(fits_path, f'{prefix_source}_c18o_mom8.fits')
+
+    # Plot mom8 (for all emissions)
+    plot_mom8(path=data_mom8_path_12co, output_path=plots_path, prefix_source=prefix_source, prefix_emission='12co', gamma=1.0, vmin=0.0, vmax=45.0)
+    plot_mom8(path=data_mom8_path_13co, output_path=plots_path, prefix_source=prefix_source, prefix_emission='13co', gamma=1.0, vmin=0.0, vmax=25.0)
+    plot_mom8(path=data_mom8_path_c18o, output_path=plots_path, prefix_source=prefix_source, prefix_emission='c18o', gamma=1.0, vmin=0.0, vmax=5.0)
+    
 
 if __name__ == '__main__':
     main()
